9c3cb98a119f38b7d2c50bbc87c398d7560d1e60
Matthieu Cottet <matthieu.cottet@outlook.com>
2025-01-18 14:34:31 +0100

    Added throttle

--- rocket.js
+++ rocket.js
@@ -4,6 +4,7 @@ class Rocket extends GravitationalObject {
         this.direction = 270;
         this.keyMap = {65: false, 68: false, 87: false};
         this.trajectory = new Trajectory(this, focus);
+        this.throttle = 1;
         document.onkeydown = document.onkeyup = this.keylogger.bind(this);
     }
 
@@ -14,7 +15,15 @@ class Rocket extends GravitationalObject {
     calculate(objects) {
         if (this.keyMap[65]) this.direction -= 0.3;
         if (this.keyMap[68]) this.direction += 0.3;
-        if (this.keyMap[87]) this.accelerate(0.02);
+        if (this.keyMap[17]) {
+            this.throttle -= 0.001;
+            if (this.throttle < 0) this.throttle = 0;
+        }
+        if (this.keyMap[16]) {
+            this.throttle += 0.001;
+            if (this.throttle > 1) this.throttle = 1;
+        }
+        if (this.keyMap[87]) this.accelerate(0.02 * this.throttle);
         super.calculate(objects);
     }
 
@@ -26,6 +35,8 @@ class Rocket extends GravitationalObject {
     draw() {
         setFillColor(this.color);
         fillRocket(this.position.x, this.position.y, this.radius * 0.7, this.radius, this.direction - 90);
+        setFillColor("#FFFFFF");
+        drawText(10, 20, "Throttle: " + this.throttle.toFixed(2), 15);
         this.trajectory.draw();
     }
 }
