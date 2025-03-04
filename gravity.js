01a9c83956ff6328358e3d554151635d3c2593eb
Matthieu Cottet <matthieu.cottet@outlook.com>
2025-03-04 21:38:20 +0100

    Changed position mars

--- gravity.js
+++ gravity.js
@@ -10,7 +10,7 @@ const rocket = new Rocket(-1.49e7, -6.37e2 - 0.0025, 0, 2.98e0);
 
 const mercury = new Planet(3.30e11, 2.44e1, "#91837D", -5.79e6, 0, 0, 4.79e0);
 const venus = new Planet(4.87e12, 6.05e1, "#D4A373", -1.08e7, 0, 0, 3.5e0);
-const mars = new Planet(6.42e11, 3.37e1, "#D99A3E", -2.28e7, 0, 0, 2.41e0);
+const mars = new Planet(6.42e11, 3.37e1, "#D99A3E", 0, 2.28e7, 2.41e0, 0);
 const jupiter = new Planet(1.90e15, 6.99e2, "#A85C43", -7.78e7, 0, 0, 1.31e0);
 const saturn = new Planet(5.68e14, 6.03e2, "#D2B16D", -1.43e8, 0, 0, 9.69e-1);
 const uranus = new Planet(8.68e13, 2.00e2, "#66A8B1", -2.87e8, 0, 0, 6.81e-1);
